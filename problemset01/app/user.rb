03d3b2a3e1a272b513aa362dd25ab087e0e295a0
shaechler <babemusicstar@hotmail.com>
2012-09-24 17:36:13 +0200

    added buy method to user

--- problemset01/app/user.rb
+++ problemset01/app/user.rb
@@ -1,5 +1,5 @@
 class User
-  attr_accessor :@name, :@credits, :@items
+  attr_accessor :name, :credits, :items
 
   def self.named (name)
     user = self.new
@@ -12,11 +12,40 @@ class User
     @items = Array.new
   end
 
-  def add_item (item)
+  def add_item (name, price)
+    item = item(name, price, self)
     @items.push(item)
   end
 
+  def del_item(item)
+    if(@items.include?item)
+      @items.delete(item)
+    end
+  end
+
+  def buy(owner, item)
+    if owner.active_items.include? item
+      if item.price<=self.credits
+        self.credits -= item.price
+        owner.credits += item.price
+        self.add_item(item, item.price)
+        owner.del_item(item)
+      else
+        puts("Not enough credits!\n")
+      end
+    else
+      puts("Invalid item!\n")
+    end
+
+  end
+
   def item_list
-    @items
+    active_items = Array.new
+    @items.each do |item|
+      if item.active?
+         active_items.push(item)
+      end
+     active_items
+    end
   end
 end
