dcd3a332603893b8fec119c6ce3e5359d2dc46eb
shaechler <babemusicstar@hotmail.com>
2012-09-26 12:31:00 +0200

    splitted tests in two

--- problemset01/test/user_test.rb
+++ problemset01/test/user_test.rb
@@ -25,42 +25,6 @@ class UserTest < Test::Unit::TestCase
     assert(user.credits==credits)
   end
 
-  def test_item_name
-    name = "Item1"
-    price = "10"
-    item = Store::Item.named(name, price)
-    assert(item.name == name)
-  end
-
-  def test_item_price
-    name = "Item1"
-    price = "10"
-    item = Store::Item.named(name, price)
-    assert(item.price == price)
-  end
-
-  def test_item_active
-    item1 = Store::Item.named("Item1", 10)
-    item2 = Store::Item.named("Item2", 5)
-
-    item1.active = false
-    item2.active=true
-
-    assert(!item1.active)
-    assert(item2.active)
-  end
-
-  def test_item_owner
-    item1 = Store::Item.named("Item1", 10)
-    item2 = Store::Item.named("Item2", 5)
-
-    user = Store::User.named("John")
-    item1.owner= user
-
-    assert(item1.owner==user)
-    assert(item2.owner==nil)
-  end
-
   def test_add_item
     user = Store::User.named("John")
     item =  Store::Item.named("test", 10)
